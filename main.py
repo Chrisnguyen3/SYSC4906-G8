7c18fd3b935a2f9712236b1d4a5b94c7835d5dca
Andrew Vicencio <andrew.v1124@gmail.com>
2022-03-11 00:45:40 -0500

    Added comments

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from tkinter import filedialog
 if __name__ == "__main__":
     scraper = Scraper()
 
+    # get file of supplies
     root = tk.Tk()
     root.withdraw()
     file_path = filedialog.askopenfilename(
@@ -18,15 +19,19 @@ if __name__ == "__main__":
         initialfile="supplies.txt"
     )
 
+    # get keywords from file
     with open(file_path, "r") as f:
         for line in f.readlines():
             keyword = line.replace("\n", "").replace("\\", "").replace("/", " ")
-            links = scraper.search(keyword)
+            links = scraper.search(keyword) # look through search page for links
 
             with open(f"raw_data/raw_{keyword}.json", "w+") as f:
                 for link in links:
-                    f.write(scraper.get_data(link) + "\n")
+                    f.write(scraper.get_data(link) + "\n") # get data from search results
             
-            pre_process(keyword)
+            pre_process(keyword) # clean a little
 
-    # scraper.quit()
+    #TODO: Add graceful error catching
+    #TODO: Add logging
+    
+    scraper.quit()
