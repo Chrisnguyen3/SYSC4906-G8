3b50b410d7973c599ae6e51b65d08db05dec7e71
Andrew Vicencio <andrew.v1124@gmail.com>
2022-03-11 15:23:04 -0500

    Added logging

--- cleaning.py
+++ cleaning.py
@@ -3,18 +3,36 @@ from datetime import datetime
 
 def pre_process(keyword):
     products = []
-    with open(f"raw_data/raw_{keyword}.json", "r") as file:
-        for line in file.readlines():
-            products.append(json.loads(line))
-    print(len(products))
 
-    with open(f"raw_data/{keyword}-pre-process.csv", "w+") as file:
-        file.write("PRODUCT, DATE, PRICE\n")
-        for product in products:
-            name = product["title"].replace("/","").replace(",", "")
-            prices = product["AMAZON"]
-            timestamp = product["timeStamp"]
-            for i in range(0, len(prices), 2):
-                temp_date = (timestamp - prices[i]) / 1000
-                date = datetime.fromtimestamp(temp_date).strftime("%Y/%m/%d")
-                file.write(f"{name}, {date}, {prices[i+1] / 100}\n")
+    try:
+        file_name = f"raw_data/raw_{keyword}.json"
+        with open(file_name, "r") as file:
+            for i, line in enumerate(file.readlines()):
+                try:
+                    products.append(json.loads(line))
+                except Exception as e:
+                    print(f"Malformed JSON on line {i} of {file_name}", e)
+    except Exception as e:
+        return print(f"Error reading raw json for {keyword}", e)
+
+    try: 
+        with open(f"raw_data/{keyword}-pre-process.csv", "w+") as file:
+            file.write("PRODUCT, DATE, PRICE\n")
+            for product in products:
+                try:
+                    name = product["title"].replace("/","").replace(",", "")
+                    prices = product["AMAZON"]
+                    timestamp = product["timeStamp"]
+                    for i in range(0, len(prices), 2):
+                        temp_date = (timestamp - prices[i]) / 1000
+                        date = datetime.fromtimestamp(temp_date).strftime("%Y/%m/%d")
+                        file.write(f"{name}, {date}, {prices[i+1] / 100}\n")
+                except Exception as e:
+                    print(f"Error pre-processing date and prices for {keyword}", e)
+    except Exception as e:
+        print(f"Error pre-processing date and prices for {keyword}", e)
+
+if __name__ == "__main__":
+    import sys
+    for arg in sys.argv[1:]:
+        pre_process(arg)
--- main.py
+++ main.py
@@ -1,28 +1,39 @@
-import os
-import time
-import json
-from scraper import Scraper
-from cleaning import pre_process
-
-import tkinter as tk
-from tkinter import filedialog
-
 if __name__ == "__main__":
-    scraper = Scraper()
+    import os
+    import sys
+    import time
+    import json
+    import logging
 
-    # get file of supplies
-    root = tk.Tk()
-    root.withdraw()
-    file_path = filedialog.askopenfilename(
-        title="Choose input file", 
-        initialdir=os.getcwd(),
-        initialfile="supplies.txt"
-    )
+    from scraper import Scraper
+    from cleaning import pre_process
 
-    # get keywords from file
-    with open(file_path, "r") as f:
-        for line in f.readlines():
-            keyword = line.replace("\n", "").replace("\\", "").replace("/", " ")
+    import tkinter as tk
+    from tkinter import filedialog
+
+    logging.basicConfig(filename='scraper.log')
+
+    try:
+        scraper = Scraper()
+
+        keywords = []
+        if (len(sys.argv) > 1):
+            keywords = sys.argv[1:]
+        else:
+            # get file of supplies
+            root = tk.Tk()
+            root.withdraw()
+            file_path = filedialog.askopenfilename(
+                title="Choose input file", 
+                initialdir=os.getcwd(),
+                initialfile="supplies.txt"
+            )
+
+            with open(file_path, "r") as f:
+                keywords = [line.replace("\n", "") for line in f.readlines()]
+
+        # get keywords from file
+        for keyword in keywords:
             links = scraper.search(keyword) # look through search page for links
 
             with open(f"raw_data/raw_{keyword}.json", "w+") as f:
@@ -30,8 +41,9 @@ if __name__ == "__main__":
                     f.write(scraper.get_data(link) + "\n") # get data from search results
             
             pre_process(keyword) # clean a little
+    except Exception as e:
+        logger.error("Error in main scraper", e)
 
     #TODO: Add graceful error catching
     #TODO: Add logging
-    
     scraper.quit()
