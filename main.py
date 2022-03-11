307ba832a1cb591cdacb8559e354702acacf91b0
Andrew Vicencio <andrew.v1124@gmail.com>
2022-03-11 00:43:41 -0500

    Added support for multi item scraping using a file

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
+import os
 import time
 import json
 from scraper import Scraper
 from cleaning import pre_process
 
+import tkinter as tk
+from tkinter import filedialog
+
 if __name__ == "__main__":
     scraper = Scraper()
 
-    print("What do you want to search? ")
-    keyword = input()
-    links = scraper.search(keyword)
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename(
+        title="Choose input file", 
+        initialdir=os.getcwd(),
+        initialfile="supplies.txt"
+    )
 
-    with open(f"raw_data/raw_{keyword}.json", "w+") as f:
-        for link in links:
-            f.write(scraper.get_data(link) + "\n")
-    
-    pre_process(keyword)
+    with open(file_path, "r") as f:
+        for line in f.readlines():
+            keyword = line.replace("\n", "").replace("\\", "").replace("/", " ")
+            links = scraper.search(keyword)
 
-    scraper.quit()
+            with open(f"raw_data/raw_{keyword}.json", "w+") as f:
+                for link in links:
+                    f.write(scraper.get_data(link) + "\n")
+            
+            pre_process(keyword)
+
+    # scraper.quit()
